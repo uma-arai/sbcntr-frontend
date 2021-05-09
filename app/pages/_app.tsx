742e8be504d0f65ca608709f476a7b0ae52d91ce
horsewin <mamamatz@gmail.com>
2021-05-09 21:29:52 +0900

    fix: layout of error boundary

--- app/pages/_app.tsx
+++ app/pages/_app.tsx
@@ -1,30 +1,38 @@
-import { AppProps, ErrorComponent, useRouter, AuthenticationError, AuthorizationError } from "blitz"
-import { ErrorBoundary, FallbackProps } from "react-error-boundary"
-import { queryCache } from "react-query"
-import LoginForm from "app/auth/components/LoginForm"
+import {
+  AppProps,
+  ErrorComponent,
+  useRouter,
+  AuthenticationError,
+  AuthorizationError,
+} from "blitz";
+import { ErrorBoundary, FallbackProps } from "react-error-boundary";
+import { queryCache } from "react-query";
+import LoginForm from "app/auth/components/LoginForm";
 
-import CssBaseline from "@material-ui/core/CssBaseline"
-import { ThemeProvider, createMuiTheme } from "@material-ui/core/styles"
-import React from "react"
+import CssBaseline from "@material-ui/core/CssBaseline";
+import { ThemeProvider, createMuiTheme } from "@material-ui/core/styles";
+import React from "react";
+import Container from "@material-ui/core/Container";
+import CopyrightComponent from "../components/molecules/copyright";
 
 //You can customize this as you want and even move it out to a separate file
 const theme = createMuiTheme({
   palette: {
     type: "light",
   },
-})
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  const getLayout = Component.getLayout || ((page) => page)
-  const router = useRouter()
+  const getLayout = Component.getLayout || ((page) => page);
+  const router = useRouter();
 
   React.useEffect(() => {
     // Remove the server-side injected CSS.
-    const jssStyles = document.querySelector("#jss-server-side")
+    const jssStyles = document.querySelector("#jss-server-side");
     if (jssStyles && jssStyles.parentElement) {
-      jssStyles.parentElement.removeChild(jssStyles)
+      jssStyles.parentElement.removeChild(jssStyles);
     }
-  }, [])
+  }, []);
 
   return (
     <ThemeProvider theme={theme}>
@@ -35,31 +43,36 @@ export default function App({ Component, pageProps }: AppProps) {
         onReset={() => {
           // This ensures the Blitz useQuery hooks will automatically refetch
           // data any time you reset the error boundary
-          queryCache.resetErrorBoundaries()
+          queryCache.resetErrorBoundaries();
         }}
       >
         {getLayout(<Component {...pageProps} />)}
       </ErrorBoundary>
     </ThemeProvider>
-  )
+  );
 }
 
 function RootErrorFallback({ error, resetErrorBoundary }: FallbackProps) {
   if (error instanceof AuthenticationError) {
-    return <LoginForm onSuccess={resetErrorBoundary} />
+    return (
+      <Container component="main" maxWidth="xs">
+        <LoginForm onSuccess={resetErrorBoundary} />
+        <CopyrightComponent />
+      </Container>
+    );
   } else if (error instanceof AuthorizationError) {
     return (
       <ErrorComponent
         statusCode={(error as any).statusCode}
         title="Sorry, you are not authorized to access this"
       />
-    )
+    );
   } else {
     return (
       <ErrorComponent
         statusCode={(error as any)?.statusCode || 400}
         title={error?.message || error?.name}
       />
-    )
+    );
   }
 }
