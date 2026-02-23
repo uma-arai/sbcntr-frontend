4300ea814a67f2229262d6bb07cb3bdefdb5199a
sf <88916645+ShoFukunaga@users.noreply.github.com>
2026-02-23 10:03:06 +0900

    Update header.tsx

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -29,7 +29,7 @@ export function Header({ className }: { className?: string }) {
       <div className="flex items-center justify-between w-full">
         <Link to="/" className="flex items-center space-x-2">
           <PawPrint className="h-6 w-6" />
-          <span className="text-xl font-bold">uma-arai Container shop</span>
+          <span className="text-xl font-bold">sf Container shop</span>
         </Link>
 
         <nav>
